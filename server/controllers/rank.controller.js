906eb90b36a8929e5ee7dd252503215dff3c7809
CharlesOsang017 <charlesosango02@gmail.com>
2026-05-16 15:16:57 +0300

    Add "addKeyword controller"

---
+++ server/controllers/rank.controller.js
@@ -0,0 +1,72 @@
+import { keywordTracking } from "../services/keywordTracking.service.js";
+
+// Add a keyword to track
+export const addKeyword = async(req, res)=>{
+    try {
+        const {keyword, url} = req.body
+        if(!keyword || !url){
+            return res.status(400).json({success: false, message: "Keyword and url are required"})
+        }
+
+        // extract domain from url
+        let domain;
+        try {
+            const parsedUrl = new URL(url.startsWith('http') ? url : `https://${url}`);
+            domain = parsedUrl.hostname.replace("www.","");
+        } catch (error) {
+            return res.status(400).json({success: false, message: "Invalid URL format"})
+        }
+
+        // check if user already tracking this keyword+domain
+        const existingKeyword = await KeywordTracking.findOne({
+            userId: req.userId,
+            keyword: keyword.toLowerCase().trim(),
+            domain
+        })
+        if(existingKeyword){
+            return res.status(400).json({success: false, message: "You are already tracking this keyword"})
+        }
+        // Create tracking entry
+        const tracking = await KeywordTracking.create({
+            userId: req.userId,
+            keyword: keyword.toLowerCase().trim(),
+            url: url.startsWith("http") ? url : `https://${url}`,
+            domain,
+            status: "checking",
+        })
+        res.status(201).json({success: true, message: "Keyword tracking started", tracking})
+        keywordTracking(tracking)
+       
+    } catch (error) {
+        console.log("Error in addKeyword controller:", error.message)
+        if(error.code === 11000){
+            return res.status(400).json({success: false, message: "You are already tracking this keyword"})
+        }
+        res.status(500).json({success: false, message: "Internal server error"})
+    }
+
+}
+// Get all tracked keywords for user
+export const getKeywords = async(req, res)=>{
+
+}
+
+// Get single keyword with full history and competitors
+export const getKeyword = async(req, res)=>{
+    
+}
+
+// Manualy refresh a keyword ranking
+export const refreshKeyword = async(req, res)=>{
+
+}
+
+// Delete a tracked keyword
+export const deleteKeyword = async(req, res)=>{
+
+}
+
+// Toggle auto-refresh for a keyword
+export const toggleAutoRefresh = async(req, res)=>{
+
+}
