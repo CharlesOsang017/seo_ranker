f3add88aefff65f3e5aee01312d0d94d0b0e0edc
CharlesOsang017 <charlesosango02@gmail.com>
2026-05-17 10:32:51 +0300

    Server redeployed

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ startRankTrackingCron()
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+    res.send("<h3>Welcome to SEO Ranker API!</h3>");
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
